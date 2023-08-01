fa9d8f7aff16258cf3625558748dff9ed0c840b9
Alfredo Deza <alfredo@deza.pe>
2023-08-01 08:04:47 -0400

    add complex parsing

--- examples/2-complex/src/main.rs
+++ examples/2-complex/src/main.rs
@@ -1,25 +1,59 @@
-use clap::{Command, Arg};
-use clap::ColorChoice;
+use clap::{Parser, ArgAction};
 use blkrs::run_lsblk;
 
-fn main() {
-    let matches = Command::new("lsblk")
-        .version("0.0.1")
-        .author("Alfredo Deza")
-        .about("lsblk in Rust")
-        .color(ColorChoice::Always)
-        .arg(
-            Arg::new("device")
-                .help("Device to query")
-                .required(true)
-                .index(1)
-        )
-        .get_matches();
+#[derive(Parser)]
+#[command(name = "lsblk", version = "0.0.1", author = "Alfredo Deza", about = "lsblk in Rust")]
+struct Opts {
+    #[clap(short, long, action = ArgAction::Count)]
+    verbose_level: u8,
 
-    if let Some(device) = matches.get_one::<String>("device") {
-        let output = serde_json::to_string(&run_lsblk(&device)).unwrap();
-        println!("{}", output);
-    } else {
-        println!("No device provided");
+    #[clap(short, long)]
+    debug: bool,
+
+    #[clap(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Parser)]
+enum Command {
+    #[clap(name = "info", about = "Get information about a device")]
+    Info(InfoOpts),
+}
+
+#[derive(Parser)]
+struct InfoOpts {
+    #[clap(help = "Device to query")]
+    device: String,
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    // Example usage of the global flags
+    if opts.debug {
+        println!("Debug mode enabled");
+    }
+
+    match opts.cmd {
+        Command::Info(info_opts) => {
+            // Example usage of the verbosity level
+            match opts.verbose_level {
+                0 => {
+                    // Quiet mode
+                }
+                1 => {
+                    println!("Running in verbose mode level 1");
+                }
+                2 => {
+                    println!("Running in verbose mode level 2");
+                }
+                3 | _ => {
+                    println!("Running in verbose mode level 3");
+                }
+            }
+
+            let output = serde_json::to_string(&run_lsblk(&info_opts.device)).unwrap();
+            println!("{}", output);
+        }
     }
 }
